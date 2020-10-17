21e2ce97ab6689bb8b4cf6bfd0bd63992c4d7899
Nicholas Segger <painn5@yahoo.com>
2020-10-17 13:37:29 -0300

    fix: Create a new instance of the block's TileEntity; Add proper identification of its properties

--- src/main/java/iamn5/shops/datagen/base/loot/table/BlockLootTablesBase.java
+++ src/main/java/iamn5/shops/datagen/base/loot/table/BlockLootTablesBase.java
@@ -1,13 +1,18 @@
 package iamn5.shops.datagen.base.loot.table;
 
+import iamn5.shops.NShops;
 import iamn5.shops.blocks.interfaces.IHasTile;
+import iamn5.shops.blocks.shop.ShopTileEntity;
+import iamn5.shops.tile.interfaces.IHasOwner;
 import iamn5.shops.tile.interfaces.IOwnable;
 import net.minecraft.block.Block;
 import net.minecraft.data.loot.BlockLootTables;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.loot.*;
 import net.minecraft.loot.functions.CopyName;
 import net.minecraft.loot.functions.CopyNbt;
 import net.minecraft.loot.functions.SetContents;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
 
@@ -28,13 +33,17 @@ public class BlockLootTablesBase extends BlockLootTables {
     protected LootTable.Builder dropSelfWithContentsAndData(Block block) {
         CopyNbt.Builder nbtBuilder = CopyNbt.builder(CopyNbt.Source.BLOCK_ENTITY);
 
-        TileEntityType<?> blockTile = null;
+        TileEntity blockTile = null;
         if (block instanceof IHasTile) {
-            blockTile = ((IHasTile<?>) block).getTileType();
+            blockTile = ((IHasTile<?>) block).getTileType().create();
         }
 
-        if (blockTile instanceof IOwnable) {
-            nbtBuilder.replaceOperation("ownerData", "BlockEntityTag.ownerID");
+        if (blockTile instanceof IHasOwner) {
+            nbtBuilder.replaceOperation("ownerData", "BlockEntityTag.ownerData");
+        }
+
+        if (blockTile instanceof IInventory) {
+            nbtBuilder.replaceOperation("Items", "BlockEntityTag.Items");
         }
 
         return LootTable.builder().addLootPool(withSurvivesExplosion(block,
@@ -43,9 +52,9 @@ public class BlockLootTablesBase extends BlockLootTables {
                         .rolls(ConstantRange.of(1))
                         .addEntry(ItemLootEntry.builder(block)
                                 .acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY))
-                                .acceptFunction(nbtBuilder)
+                                .acceptFunction(nbtBuilder)))
                                 .acceptFunction(SetContents.builderIn()
-                                        .addLootEntry(DynamicLootEntry.func_216162_a(new ResourceLocation("minecraft", "contents"))))))
+                                    .addLootEntry(DynamicLootEntry.func_216162_a(new ResourceLocation("minecraft", "contents"))))
         );
     }
 }
