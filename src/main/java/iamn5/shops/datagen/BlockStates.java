75573b0e5e44b8a595b25d0c7931d8291eb65914
Nicholas Segger <painn5@yahoo.com>
2020-10-17 13:33:14 -0300

    feat: Proper "directional" block implementation

--- src/main/java/iamn5/shops/datagen/BlockStates.java
+++ src/main/java/iamn5/shops/datagen/BlockStates.java
@@ -2,11 +2,19 @@ package iamn5.shops.datagen;
 
 import iamn5.shops.NShops;
 import iamn5.shops.init.Registration;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.util.function.Function;
 
 public class BlockStates extends BlockStateProvider {
     public BlockStates(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -19,12 +27,24 @@ public class BlockStates extends BlockStateProvider {
     }
 
     private void registerShop() {
-        ResourceLocation front = new ResourceLocation(NShops.MOD_ID, "textures/block/shop/front");
-        ResourceLocation right = new ResourceLocation(NShops.MOD_ID, "textures/block/shop/right");
-        ResourceLocation left = new ResourceLocation(NShops.MOD_ID, "textures/block/shop/left");
-        ResourceLocation back = new ResourceLocation(NShops.MOD_ID, "textures/block/shop/back");
+        ResourceLocation front = new ResourceLocation(NShops.MOD_ID, "block/shop/front");
+        ResourceLocation right = new ResourceLocation(NShops.MOD_ID, "block/shop/right");
+        ResourceLocation left = new ResourceLocation(NShops.MOD_ID, "block/shop/left");
+        ResourceLocation back = new ResourceLocation(NShops.MOD_ID, "block/shop/back");
 
         BlockModelBuilder modelShop = models().cube("shop", back, back, front, back, left, right);
-        directionalBlock(Registration.SHOP_BLOCK.get(), blockState -> modelShop);
+        orientedBlock(Registration.SHOP_BLOCK.get(), blockState -> modelShop);
+    }
+
+    private void orientedBlock(Block block, Function<BlockState, ModelFile> modelFunc) {
+        getVariantBuilder(block)
+                .forAllStates(state -> {
+                    Direction dir = state.get(BlockStateProperties.FACING);
+                    return ConfiguredModel.builder()
+                            .modelFile(modelFunc.apply(state))
+                            .rotationX(dir.getAxis() == Direction.Axis.Y ?  dir.getAxisDirection().getOffset() * -90 : 0)
+                            .rotationY(dir.getAxis() != Direction.Axis.Y ? ((dir.getHorizontalIndex() + 2) % 4) * 90 : 0)
+                            .build();
+                });
     }
 }
